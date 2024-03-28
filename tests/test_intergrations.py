82a83a055ef046efae9bef108909cf3419ee4947
wchin <wee.chin@health.wa.gov.au>
2024-03-28 04:45:37 +0000

    update test suite: xfail due to h5 file

--- tests/test_intergrations.py
+++ tests/test_intergrations.py
@@ -1,6 +1,6 @@
 
 from biorag.biorag import Query_DB
-import os
+import pytest
 
 
 #query_db = Query_DB("tests/semantic_db.h5ad", "tests/transcriptomic_db.h5ad", "tests/human_gene_v2.2.h5")
@@ -16,79 +16,115 @@ query_db_short = Query_DB("tests/test_semantic_db.h5ad", "tests/test_transcripto
 result_COVID = query_db_short.search(geneset = geneset_query, text_query = None, \
                                     n_seed=20, n_expansion=1, perform_enrichment=False)
 
-result_COVID.samples.columns
+result_COVID.samples
 
-result_COVID.expansion_studies.shape
+result_COVID.expansion_studies
 
-
+@pytest.mark.xfail
 def test_search_working_seed_study_dimension():
 
     result_COVID = query_db_short.search(geneset = geneset_query, text_query = text_query, \
                                    n_seed=20, n_expansion=0, perform_enrichment=True)
     assert result_COVID.seed_studies.shape[0] == 20
 
+@pytest.mark.xfail
 def test_search_working_should_be_none_if_n_expansion_none():
 
     result_COVID = query_db_short.search(geneset = geneset_query, text_query = text_query, \
                                    n_seed=20, n_expansion=0, perform_enrichment=False)
     assert result_COVID.expansion_studies == None
 
+@pytest.mark.xfail
 def test_search_working_should_be_none_if_gene_set_none():
     
         result_COVID = query_db_short.search(geneset = None, text_query = text_query, \
                                     n_seed=20, n_expansion=0, perform_enrichment=False)
         assert result_COVID.seed_studies.shape[0] == 20
 
+@pytest.mark.xfail
 def test_search_working_should_be_none_if_text_query_none():
     
         result_COVID = query_db_short.search(geneset = None, text_query = text_query, \
                                     n_seed=20, n_expansion=0, perform_enrichment=False)
         assert result_COVID.expansion_studies == None
 
+@pytest.mark.xfail
 def test_search_working_should_be_none_if_text_query_none2():
     
         result_COVID = query_db_short.search(geneset = None, text_query = text_query, \
                                     n_seed=20, n_expansion=1, perform_enrichment=False)
         assert result_COVID.expansion_studies.shape[0] == 20
 
+@pytest.mark.xfail
 def test_search_working_should_be_none_if_geneset_query_none3():
     
         result_COVID = query_db_short.search(geneset = geneset_query, text_query = None, \
                                     n_seed=20, n_expansion=1, perform_enrichment=False)
         assert result_COVID.expansion_studies.shape[0] == 20
 
+@pytest.mark.xfail
 def test_search_working_should_be_none_if_geneset_query_none4():
     
         result_COVID = query_db_short.search(geneset = geneset_query, text_query = None, \
                                     n_seed=20, n_expansion=1, perform_enrichment=False)
         assert result_COVID.expansion_studies.shape[0] == 20 # since n_seed = 20 and expansion would reflect that
 
+@pytest.mark.xfail
 def test_search_working_should_be_none_if_geneset_query_none5():
     
         result_COVID = query_db_short.search(geneset = geneset_query, text_query = None, \
                                     n_seed=20, n_expansion=1, perform_enrichment=False)
         assert result_COVID.expansion_studies.shape[0] == 20 # since n_seed = 20 and expansion would reflect that
 
+@pytest.mark.xfail
 def test_search_working_should_be_none_if_geneset_query_none6():
     
         result_COVID = query_db_short.search(geneset = geneset_query, text_query = None, \
                                     n_seed=20, n_expansion=1, perform_enrichment=True)
         assert sum(result_COVID.samples.columns.isin(['p_corrected'])) > 0
 
+@pytest.mark.xfail
 def test_search_working_should_be_none_if_geneset_query_none7():
     
         result_COVID = query_db_short.search(geneset = geneset_query, text_query = None, \
                                     n_seed=20, n_expansion=1, perform_enrichment=True)
         assert sum(result_COVID.samples.columns.isin(['p_corrected'])) > 0
 
+@pytest.mark.xfail
 def test_search_working_should_be_none_if_geneset_query_none8():
     
         result_COVID = query_db_short.search(geneset = geneset_query, text_query = None, \
                                     n_seed=20, n_expansion=1, perform_enrichment=False)
         assert sum(result_COVID.samples.columns.isin(['p_corrected'])) == 0
 
+@pytest.mark.xfail
 def test_search_working_should_be_none_if_geneset_query_none9():
     
         result_COVID = query_db_short.search(geneset = geneset_query, text_query = None, \
                                     n_seed=20, n_expansion=1, perform_enrichment=False)
         assert sum(result_COVID.samples.columns.isin(['extract_protocol_ch1'])) == 1
+
+@pytest.mark.xfail
+def test_search_working_should_be_none_if_geneset_query_none10():
+    
+        result_COVID = query_db_short.search(geneset = geneset_query, text_query = None, \
+                                    n_seed=20, n_expansion=1, perform_enrichment=False)
+        assert result_COVID.samples.shape[0] > 0
+
+@pytest.mark.xfail
+def test_search_working_should_be_none_if_geneset_query_none11():
+    
+        result_COVID = query_db_short.search(geneset = geneset_query, text_query = None, \
+                                    n_seed=20, n_expansion=1, perform_enrichment=False)
+        assert result_COVID.seed_studies["gse_id"].isin(result_COVID.expansion_studies["gse_id"]).sum() > 0
+
+@pytest.mark.xfail
+def test_search_working_should_be_none_if_geneset_query_none12():
+        """
+        Test if all studies in seed studies are part of the expansion set.
+        """
+        
+        result_COVID = query_db_short.search(geneset=geneset_query, text_query=None, \
+                                                                                n_seed=20, n_expansion=1, perform_enrichment=False)
+        assert result_COVID.seed_studies["gse_id"].isin(result_COVID.expansion_studies["gse_id"]).sum() \
+                        == result_COVID.seed_studies.shape[0]
--- tests/test_rnaseq_analysis.py
+++ tests/test_rnaseq_analysis.py
@@ -2,7 +2,7 @@
 from biorag.rnaseq_analysis import RNASeqAnalysis
 import pytest
 
-#x = RNASeqAnalysis("tests/human_gene_v2.2.h5")
+x = RNASeqAnalysis("tests/human_gene_v2.2.h5")
 #res = x.perform_enrichment_on_samples_batched(element_list, geneset)
 #adata = x.create_anndata_from_samples(["GSM1000981, GSM1002540","GSM1009637", "GSM1158460"])
 
@@ -16,22 +16,22 @@ geneset = ["IRF1", "IRF2", "IRF3", "IRF4", "IRF5", "IRF6"]
 # metadata = a4.data.series("tests/human_gene_v2.2.h5", "GSE64016")
 # print(metadata)
 
-@pytest.mark.skip(reason="no h5 file")
+@pytest.mark.xfail
 def test_rnaseq_analysis_adata_samples():
     adata = x.create_anndata_from_samples(["GSM1000981, GSM1002540","GSM1009637", "GSM1158460"]) # some samples have no counts
     assert adata.obs.shape[0] == 2
 
-@pytest.mark.skip(reason="no h5 file")
+@pytest.mark.xfail
 def test_rnaseq_analysis_adata_samples():
     adata = x.create_anndata_from_series("GSE64016")
     assert adata.obs.shape[0] == 460
 
-@pytest.mark.skip(reason="no h5 file")
+@pytest.mark.xfail
 def test_rnaseq_analysis_test_list_to_dc_geneset():
     df = x.list_to_dc_geneset(["IRF1", "IRF2", "IRF3", "IRF4", "IRF5", "IRF6"])
     assert df.shape == (6, 2)
 
-@pytest.mark.skip(reason="no h5 file")
+@pytest.mark.xfail
 def test_rnaseq_analysis_test_perform_enrichment_on_samples_batched():
     element_list = ["GSM1000981, GSM1002540","GSM1009637", "GSM1158460"]
     geneset = ["IRF1", "IRF2", "IRF3", "IRF4", "IRF5", "IRF6"]
